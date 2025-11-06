1559a92447ee21a475928cee97303aa0b5c66068
Pablo Vega <pablovega6543@gmail.com>
2025-11-06 18:15:08 -0300

    aa

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
               class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4"
             >
               <div>
-                <h3 class="fw-bold mb-3">Dashboard</h3>
+                <h3 class="fw-bold mb-3">Hola Waso</h3>
                 <h6 class="op-7 mb-2">Free Bootstrap 5 Admin Dashboard</h6>
               </div>
               <div class="ms-md-auto py-2 py-md-0">
